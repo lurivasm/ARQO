7e283d8250382476ed732e00f22ab1bba4fe2409
danist7 <32202733+danist7@users.noreply.github.com>
2018-09-27 13:02:54 +0200

    dghsfnfn

--- arq0_p1/rtl/control_unit.vhd
+++ arq0_p1/rtl/control_unit.vhd
@@ -20,6 +20,7 @@ entity control_unit is
       Funct : in std_logic_vector (5 downto 0);
       -- Seniales para el PC
       Branch : out  std_logic; -- 1=Ejecutandose instruccion branch
+      Jump : out std_logic;   -- 1=Ejecutandose instruccion jump
       -- Seniales relativas a la memoria
       MemToReg : out  std_logic; -- 1=Escribir en registro la salida de la mem.
       MemWrite : out  std_logic; -- Escribir la memoria
@@ -45,19 +46,30 @@ architecture rtl of control_unit is
    constant OP_LW     : t_opCode := "100011";
    constant OP_LUI    : t_opCode := "001111";
    constant OP_ADDI   : t_opCode := "001000";
+   constant OP_JUMP   : t_opCode := "000010";
+   constant OP_SLTI   : t_opCode := "001010";
 
 begin
   process(OpCode)
     begin
-      if OpCode = "000010" then  -- Instucción j
-        
+      if OpCode = OP_JUMP then  -- Instucción j
+            MemToReg  <= '0';
+						MemWrite  <= '0';
+						MemRead <= '0';
+						Branch <= '0';
+						Jump <= '1';
+						ALUControl <= "0000";
+						ALUSrc <= '0';
+						RegDst <= '0';
+						RegWrite <= '0';
       
       elsif OpCode = OP_BEQ then   --Instrucción beq
 						MemToReg  <= '0';
 						MemWrite  <= '0';
 						MemRead <= '0';
 						Branch <= '1';
-						ALUControl <= "0001";
+						Jump <= '0';
+						ALUControl <= "1010";
 						ALUSrc <= '0';
 						RegDst <= '0';
 						RegWrite <= '0';
@@ -67,6 +79,7 @@ begin
 						MemWrite  <= '0';
 						MemRead <= '0';
 						Branch <= '0';
+						Jump <= '0';
 						ALUControl <= "0000";
 						ALUSrc <= '1';
 						RegDst <= '0';
@@ -77,6 +90,7 @@ begin
 						MemWrite  <= '0';
 						MemRead <= '1';
 						Branch <= '0';
+						Jump <= '0';
 						ALUControl <= "0000";
 						ALUSrc <= '1';
 						RegDst <= '0';
@@ -87,14 +101,72 @@ begin
 						MemWrite  <= '1';
 						MemRead <= '0';
 						Branch <= '0';
+						Jump <= '0';
 						ALUControl <= "0000";
 						ALUSrc <= '1';
 						RegDst <= '0';
 						RegWrite <= '0';
 						
+			elsif OpCode = OP_LUI then   --Instrucción lui
+						MemToReg  <= '0';
+						MemWrite  <= '0';
+						MemRead <= '0';
+						Branch <= '0';
+						Jump <= '0';
+						ALUControl <= "1101";
+						ALUSrc <= '1';
+						RegDst <= '0';
+						RegWrite <= '1';
 						
-					
+			elsif OpCode = OP_SLTI then   --Instrucción slti
+						MemToReg  <= '0';
+						MemWrite  <= '0';
+						MemRead <= '0';
+						Branch <= '0';
+						Jump <= '0';
+						ALUControl <= "1010";
+						ALUSrc <= '1';
+						RegDst <= '0';
+						RegWrite <= '1';
+						
+			elsif (OpCode = OP_RTYPE) and (Funct /= "000000") then   --Instrucciones tipo R
+						MemToReg  <= '0';
+						MemWrite  <= '0';
+						MemRead <= '0';
+						Branch <= '0';
+						Jump <= '0';
+						ALUSrc <= '0';
+						RegDst <= '1';
+						RegWrite <= '1';
+						
+						if Funct = "100000" then  -- add
+						  ALUControl <= "0000";
+						  
+						elsif Funct = "100100" then -- and
+						  ALUControl <= "0100";
+						
+						elsif Funct = "100101" then -- or
+						  ALUControl <= "0111";
+						
+						elsif Funct = "100010" then -- sub
+						  ALUControl <= "0001";
+						
+						elsif Funct = "100110" then -- xor
+						  ALUControl <= "0110";
+						end if;
+				
+			elsif (OpCode = OP_RTYPE) and (Funct = "000000") then -- Instrucción NOP	
+			      MemToReg  <= '0';
+						MemWrite  <= '0';
+						MemRead <= '0';
+						Branch <= '0';
+						Jump <= '0';
+						ALUControl <= "0000";
+						ALUSrc <= '0';
+						RegDst <= '0';
+						RegWrite <= '0';								 
         
-        
+      end if;  
+  end process;
 
 end architecture;
