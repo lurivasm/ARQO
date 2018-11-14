98a29b18fb35650a4f312c96fdcd22c619eca9fa
lurivasm <lucia_rm98@hotmail.com>
2018-11-14 12:57:56 +0100

    ej2

--- Practica 3/ejercicio2.sh
+++ Practica 3/ejercicio2.sh
@@ -4,51 +4,51 @@
 Ninicio=6096
 Npaso=64
 Nfinal=7120
-fDAT=slow_fast_time.dat
-fPNG=slow_fast_time.png
+rpng=cache_lectura.png
+wpng=cache_escritura.png
 tamCache=(1024 2048 4096 8192)
 contador=0
-
+D1mr_slow=()
+D1mw_slow=()
+D1mr_fast=()
+D1mw_fast=()
 
 # borrar el fichero DAT y el fichero PNG
-rm -f slow_cachegrind.dat fast_cachegrind.dat $fDAT fPNG
-
-# generar el fichero DAT vacío
-touch $fDAT
+rm -f slow_cachegrind.dat fast_cachegrind.dat $rpng wpng
 
 # Establecemos los valores para la cache de ultimo nivel
-# Nivel 1 de instrucciones, nivel 1 de datos y ultimo nivel
 # Size, associative, line size (bytes)
 valgrind --tool=cachegrind --LL=8388608,1,64
 
-echo "Running slow and fast..."
-# bucle para N desde 2000+1024*4 hasta 2000+1024*5
+echo "Ejercicio 2 : Running slow and fast..."
+# bucle que varia el tamaño de la matriz desde 2000+1024*4 hasta 2000+1024*5
 #for N in $(seq $Ninicio $Npaso $Nfinal);
 for ((N = Ninicio ; N <= Nfinal ; N += Npaso));
 do
 	echo "N: $N / $Nfinal..."
 
+   # Bucle para variar el tamaño de la cache
    for ((i=0; i<${#tamCache[*]}; i++));
    do
+      # Configuramos la cache de nivel 1 de instrucciones y de datos
       valgrind --tool=cachegrind --I1=${tamCache[i]},1,64
       valgrind --tool=cachegrind --D1=${tamCache[i]},1,64
-   	echo "cache_${tamCache[i]}.dat"
+
+      # Ejecutamos valgrind y generamos el fichero
+      valgrind --tool=cachegrind  --cachegrind-out-file=slow_cachegrind.dat ./slow $N
+      valgrind --tool=cachegrind  --cachegrind-out-file=fast_cachegrind.dat ./fast $N
+
+      D1mr_slow[contador]=$(cg_annotate slow_cachegrind.dat | head -n 22 | grep 'PROGRAM TOTALS' | awk '{print $5}')
+      D1mw_slow[contador]=$(cg_annotate slow_cachegrind.dat | head -n 22 | grep 'PROGRAM TOTALS' | awk '{print $8}')
+
+      D1mr_fast[contador]=$(cg_annotate fast_cachegrind.dat | head -n 22 | grep 'PROGRAM TOTALS' | awk '{print $5}')
+      D1mw_fast[contador]=$(cg_annotate fast_cachegrind.dat | head -n 22 | grep 'PROGRAM TOTALS' | awk '{print $8}')
+      contador ++
+
+   	echo "$N $D1mr $D1mw $D1mr $D1mw" >> cache_${tamCache[i]}.dat
+
    done
 
-	# ejecutar los programas slow y fast consecutivamente con tamaño de matriz N
-	# para cada uno, filtrar la línea que contiene el tiempo y seleccionar la
-	# tercera columna (el valor del tiempo). Dejar los valores en variables
-	# para poder imprimirlos en la misma línea del fichero de datos
-	slowTime=$(./slow $N | grep 'time' | awk '{print $3}')
-	fastTime=$(./fast $N | grep 'time' | awk '{print $3}')
-
-   # Ejecutamos valgrind y generamos el fichero
-   valgrind --tool=cachegrind  --cachegrind-out-file=slow_cachegrind.dat ./slow $N
-   valgrind --tool=cachegrind  --cachegrind-out-file=fast_cachegrind.dat ./fast $N
-   cg_annotate slow_cachegrind.dat | head -n 22 | grep 'PROGRAM TOTALS' | awk '{print $1}'
-
-
-	echo "$N	$slowTime	$fastTime" >> $fDAT
 done
 
 # Generamos los graficos con gnuplot
