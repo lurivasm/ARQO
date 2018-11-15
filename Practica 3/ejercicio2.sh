b4b3182a1b009fc5326e3fc393e3e74582208756
danist7 <daniel.santo-tomas@estudiante.uam.es>
2018-11-15 20:54:06 +0100

    Ahi vamos

--- Practica 3/ejercicio2.sh
+++ Practica 3/ejercicio2.sh
@@ -14,38 +14,34 @@ declare -a D1mr_fast
 declare -a D1mw_fast
 
 # borrar el fichero DAT y el fichero PNG
-rm -f $rpng wpng
+rm -f $rpng $wpng cache_1024.dat cache_2048.dat cache_4096.dat cache_8192.dat
 
-# Establecemos los valores para la cache de ultimo nivel
-# Size, associative, line size (bytes)
-valgrind --tool=cachegrind --LL=8388608,1,64
 
 echo "Ejercicio 2 : Running slow and fast..."
 # bucle que varia el tamaño de la matriz desde 2000+1024*4 hasta 2000+1024*5
-#for N in $(seq $Ninicio $Npaso $Nfinal);
-for ((N = Ninicio ; N <= Nfinal ; N += Npaso));
+
+for ((N = Ninicio ; N <=Nfinal ; N += Npaso));
 do
 	echo "N: $N / $Nfinal..."
 
    # Bucle para variar el tamaño de la cache
    for ((i=0; i<${#tamCache[*]}; i++));
    do
-      # Configuramos la cache de nivel 1 de instrucciones y de datos
-      valgrind --tool=cachegrind --I1=${tamCache[i]},1,64
-      valgrind --tool=cachegrind --D1=${tamCache[i]},1,64
 
-      # Ejecutamos valgrind y generamos el fichero
-      valgrind --tool=cachegrind  --cachegrind-out-file=slow_cachegrind.dat ./slow $N
-      valgrind --tool=cachegrind  --cachegrind-out-file=fast_cachegrind.dat ./fast $N
+      # Ejecutamos valgrind con la configuración de cache pedida y generamos el fichero
+      valgrind --tool=cachegrind --LL=8388608,1,64 --I1=${tamCache[$i]},1,64 --D1=${tamCache[$i]},1,64 --cachegrind-out-file=slow_cachegrind.dat ./slow $N
+      valgrind --tool=cachegrind --LL=8388608,1,64 --I1=${tamCache[$i]},1,64 --D1=${tamCache[$i]},1,64 --cachegrind-out-file=fast_cachegrind.dat ./fast $N
 
-      D1mr_slow[contador]=$(cg_annotate slow_cachegrind.dat | head -n 22 | grep 'PROGRAM TOTALS' | awk '{print $5}')
-      D1mw_slow[contador]=$(cg_annotate slow_cachegrind.dat | head -n 22 | grep 'PROGRAM TOTALS' | awk '{print $8}')
+      D1mr_slow[$contador]=$(cg_annotate slow_cachegrind.dat | head -n 22 | grep 'PROGRAM TOTALS' | awk '{print $5}'| sed -e 's/,//g')
+			echo "coso"${D1mr_slow[$contador]}
+      D1mw_slow[$contador]=$(cg_annotate slow_cachegrind.dat | head -n 22 | grep 'PROGRAM TOTALS' | awk '{print $8}'| sed -e 's/,//g')
 
-      D1mr_fast[contador]=$(cg_annotate fast_cachegrind.dat | head -n 22 | grep 'PROGRAM TOTALS' | awk '{print $5}')
-      D1mw_fast[contador]=$(cg_annotate fast_cachegrind.dat | head -n 22 | grep 'PROGRAM TOTALS' | awk '{print $8}')
-      ((contador++))
+      D1mr_fast[$contador]=$(cg_annotate fast_cachegrind.dat | head -n 22 | grep 'PROGRAM TOTALS' | awk '{print $5}'| sed -e 's/,//g')
+      D1mw_fast[$contador]=$(cg_annotate fast_cachegrind.dat | head -n 22 | grep 'PROGRAM TOTALS' | awk '{print $8}'| sed -e 's/,//g')
 
-   	echo "$N ${D1mr_slow[$contador]} ${D1mw_slow[$contador]} ${D1mr_fast[$contador]} ${D1mw_fast[$contador]}" >> cache_${tamCache[i]}.dat
+   		echo $N ${D1mr_slow[$contador]} ${D1mw_slow[$contador]} ${D1mr_fast[$contador]} ${D1mw_fast[$contador]} >> cache_${tamCache[$i]}.dat
+
+			((contador++))
 
    done
 
@@ -57,13 +53,39 @@ echo "Generating plot..."
 gnuplot << END_GNUPLOT
 set title "Fallos de lectura"
 set ylabel "Fallos de Caché"
-set xlabel "Tamaño de Caché"
+set xlabel "Tamaño de Matriz"
 set key right bottom
 set grid
 set term png
 set output "$rpng"
-plot "cache_1024" using 1:2 with lines lw 2 title "slow", \
-     "$fDAT" using 1:3 with lines lw 2 title "fast"
+plot "cache_1024.dat" using 1:2 with lines lw 2 title "1024 slow", \
+     "cache_1024.dat" using 1:4 with lines lw 2 title "1024 fast", \
+		 "cache_2048.dat" using 1:2 with lines lw 2 title "2048 slow", \
+		 "cache_2048.dat" using 1:4 with lines lw 2 title "2048 fast", \
+		 "cache_4096.dat" using 1:2 with lines lw 2 title "4096 slow", \
+		 "cache_4096.dat" using 1:4 with lines lw 2 title "4096 fast", \
+		 "cache_8192.dat" using 1:2 with lines lw 2 title "8192 slow", \
+		 "cache_8192.dat" using 1:4 with lines lw 2 title "8192 fast"
+replot
+quit
+END_GNUPLOT
+
+gnuplot << END_GNUPLOT
+set title "Fallos de escritura"
+set ylabel "Fallos de Caché"
+set xlabel "Tamaño de Matriz"
+set key right bottom
+set grid
+set term png
+set output "$wpng"
+plot "cache_1024.dat" using 1:3 with lines lw 2 title "1024 slow", \
+     "cache_1024.dat" using 1:5 with lines lw 2 title "1024 fast", \
+		 "cache_2048.dat" using 1:3 with lines lw 2 title "2048 slow", \
+		 "cache_2048.dat" using 1:5 with lines lw 2 title "2048 fast", \
+		 "cache_4096.dat" using 1:3 with lines lw 2 title "4096 slow", \
+		 "cache_4096.dat" using 1:5 with lines lw 2 title "4096 fast", \
+		 "cache_8192.dat" using 1:3 with lines lw 2 title "8192 slow", \
+		 "cache_8192.dat" using 1:5 with lines lw 2 title "8192 fast"
 replot
 quit
 END_GNUPLOT
