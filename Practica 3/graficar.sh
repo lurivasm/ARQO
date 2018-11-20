44ff4af85030d341dd2a25804dd66fee68c0e43d
danist7 <daniel.santo-tomas@estudiante.uam.es>
2018-11-20 16:17:57 +0100

    orden y justicia

--- Practica 3/graficar.sh
+++ Practica 3/graficar.sh
@@ -1,32 +1,17 @@
 #!/bin/bash
 # Generamos los graficos con gnuplot
-echo "Generating plot..."
+fPNG=slow_fast_time.png
+fDAT=slow_fast_time.dat
 gnuplot << END_GNUPLOT
-set title "Fallos de Caché"
-set ylabel "Numero de fallos"
-set xlabel "Tamaño de Matriz"
-set key outside
-set grid
-set term png
-set output "mult_cache.png"
-plot "mult.dat" using 1:3 with lines lw 2 title "Lectura(mult)", \
-     "mult.dat" using 1:4 with lines lw 2 title "Escritura(mult)", \
-		 "mult.dat" using 1:6 with lines lw 2 title "Lectura(tras)", \
-		 "mult.dat" using 1:7 with lines lw 2 title "Escritura(tras)"
-replot
-quit
-END_GNUPLOT
-
-gnuplot << END_GNUPLOT
-set title "Tiempo de ejecucion"
+set title "Tiempo de Ejecución Slow-Fast "
 set ylabel "Tiempo(s)"
 set xlabel "Tamaño de Matriz"
-set key outside
+set key right bottom
 set grid
 set term png
-set output "mult_time.png"
-plot "mult.dat" using 1:2 with lines lw 2 title "Mult", \
-     "mult.dat" using 1:5 with lines lw 2 title "Tras"
+set output "$fPNG"
+plot "$fDAT" using 1:2 with lines lw 2 title "slow", \
+     "$fDAT" using 1:3 with lines lw 2 title "fast"
 replot
 quit
 END_GNUPLOT
--- Practica 3/prueba.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-valgrind --tool=cachegrind  --LL=8388608,1,64 --I1=1024,1,64 --D1=1024,1,64 --cachegrind-out-file=slow_cachegrind.dat ./slow 4
